71daa1f1f86cf0cb1c8714f99e006ab7eab26f94
Emiliano Errecalde <eerrecalde@gmail.com>
2016-08-11 11:58:11 +0100

    Added main config file to be used by both dev and prod

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,18 +1,10 @@
 import webpack from 'webpack'
 import path from 'path'
-import cssnano from 'cssnano'
+import config from './webpack.config'
 
 const baseUrl = path.join(__dirname, '..')
 
-let PATHS_TO_TREAT_AS_CSS_MODULES = [
-  path.join(baseUrl, 'src').replace(/[\^\$\.\*\+\-\?\=\!\:\|\\\/\(\)\[\]\{\}\,]/g, '\\$&') // eslint-disable-line
-]
-
-const isUsingCSSModules = !!PATHS_TO_TREAT_AS_CSS_MODULES.length
-const cssModulesRegex = new RegExp(`(${PATHS_TO_TREAT_AS_CSS_MODULES})`)
-console.log('LALALA', isUsingCSSModules)
-
-const webpackConfig = {
+const webpackConfigDev = {
   debug: true,
   devtool: 'cheap-module-eval-source-map',
   noInfo: false,
@@ -21,98 +13,10 @@ const webpackConfig = {
     'webpack-hot-middleware/client?reload=true', // note that it reloads the page if hot module reloading fails.
     path.join(baseUrl, 'src', 'index')
   ],
-  target: 'web',
-  output: {
-    path: path.join(baseUrl, 'dist'), // Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.join(baseUrl, 'src')
-  },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
-  ],
-  module: {
-    loaders: [
-      {test: /\.js$/, include: path.join(baseUrl, 'src'), loaders: ['babel']},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
-    ]
-  }
+  ]
 }
 
-if (isUsingCSSModules) {
-  const cssModulesLoader = [
-    'css?sourceMap&-minimize',
-    'modules',
-    'importLoaders=1',
-    'localIdentName=[name]-[local]-[hash:base64:5]'
-  ].join('&')
-
-  webpackConfig.module.loaders.push({
-    test: /\.scss$/,
-    include: cssModulesRegex,
-    loaders: [
-      'simple-universal-style',
-      cssModulesLoader,
-      'postcss',
-      'sass?sourceMap'
-    ]
-  })
-
-  webpackConfig.module.loaders.push({
-    test: /\.css$/,
-    include: cssModulesRegex,
-    loaders: [
-      'simple-universal-style',
-      cssModulesLoader,
-      'postcss'
-    ]
-  })
-}
-
-// Loaders for files that should not be treated as CSS modules.
-const excludeCSSModules = isUsingCSSModules ? cssModulesRegex : false
-webpackConfig.module.loaders.push({
-  test: /\.scss$/,
-  exclude: excludeCSSModules,
-  loaders: [
-    'simple-universal-style',
-    'css?sourceMap&-minimize',
-    'postcss',
-    'sass?sourceMap'
-  ]
-})
-webpackConfig.module.loaders.push({
-  test: /\.css$/,
-  exclude: excludeCSSModules,
-  loaders: [
-    'simple-universal-style',
-    'css?sourceMap&-minimize',
-    'postcss'
-  ]
-})
-
-webpackConfig.postcss = [
-  cssnano({
-    autoprefixer: {
-      add: true,
-      remove: true,
-      browsers: ['last 2 versions']
-    },
-    discardComments: {
-      removeAll: true
-    },
-    discardUnused: false,
-    mergeIdents: false,
-    reduceIdents: false,
-    safe: true,
-    sourcemap: true
-  })
-]
-
-export default webpackConfig
+export default Object.assign(config, webpackConfigDev)
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,7 +1,7 @@
 import webpack from 'webpack'
 import path from 'path'
 import ExtractTextPlugin from "extract-text-webpack-plugin"
-import cssnano from 'cssnano'
+import config from './webpack.config'
 
 // GLOBALS to tell react to work on prod mode.
 const GLOBALS = {
@@ -10,128 +10,18 @@ const GLOBALS = {
 
 const baseUrl = path.join(__dirname, '..')
 
-let PATHS_TO_TREAT_AS_CSS_MODULES = [
-  path.join(baseUrl, 'src').replace(/[\^\$\.\*\+\-\?\=\!\:\|\\\/\(\)\[\]\{\}\,]/g, '\\$&') // eslint-disable-line
-]
-
-const isUsingCSSModules = !!PATHS_TO_TREAT_AS_CSS_MODULES.length
-const cssModulesRegex = new RegExp(`(${PATHS_TO_TREAT_AS_CSS_MODULES})`)
-
-const webpackConfigProd = {
+const webpackConfig = {
   debug: true,
-  // recommended prod dev tools.. source-map
-  // devtool: 'cheap-module-eval-source-map',
   devtool: 'source-map',
   noInfo: false,
-  // Removing hot reloading and polyfill for prod. We'll only leave the index file as you'll see below.
-  // entry: [
-  //   'eventsource-polyfill', // necessary for hot reloading with IE
-  //   'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-  //   './src/index'
-  // ],
   entry: path.join(baseUrl, 'src', 'index'),
-  target: 'web',
-  output: {
-    path: path.join(baseUrl, 'dist'), // Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  // Change the serving files directory from src to dist
-  devServer: {
-    // contentBase: './src'
-    contentBase: path.join(baseUrl, 'dist')
-  },
-  // No need for these 2 plugins below:
-  // plugins: [
-  //   new webpack.HotModuleReplacementPlugin(),
-  //   new webpack.NoErrorsPlugin()
-  // ],
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin(GLOBALS),
     new ExtractTextPlugin('styles.css'),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin()
-  ],
-  module: {
-    loaders: [
-      {test: /\.js$/, include: path.join(baseUrl, 'src'), loaders: ['babel']},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
-    ]
-  }
+  ]
 }
 
-if (isUsingCSSModules) {
-  const cssModulesLoader = [
-    'css?sourceMap&-minimize',
-    'modules',
-    'importLoaders=1',
-    'localIdentName=[name]-[local]-[hash:base64:5]'
-  ].join('&')
-
-  webpackConfigProd.module.loaders.push({
-    test: /\.scss$/,
-    include: cssModulesRegex,
-    loaders: [
-      'simple-universal-style',
-      cssModulesLoader,
-      'postcss',
-      'sass?sourceMap'
-    ]
-  })
-
-  webpackConfigProd.module.loaders.push({
-    test: /\.css$/,
-    include: cssModulesRegex,
-    loaders: [
-      'simple-universal-style',
-      cssModulesLoader,
-      'postcss'
-    ]
-  })
-}
-
-// Loaders for files that should not be treated as CSS modules.
-const excludeCSSModules = isUsingCSSModules ? cssModulesRegex : false
-webpackConfigProd.module.loaders.push({
-  test: /\.scss$/,
-  exclude: excludeCSSModules,
-  loaders: [
-    'simple-universal-style',
-    'css?sourceMap&-minimize',
-    'postcss',
-    'sass?sourceMap'
-  ]
-})
-webpackConfigProd.module.loaders.push({
-  test: /\.css$/,
-  exclude: excludeCSSModules,
-  loaders: [
-    'simple-universal-style',
-    'css?sourceMap&-minimize',
-    'postcss'
-  ]
-})
-
-webpackConfigProd.postcss = [
-  cssnano({
-    autoprefixer: {
-      add: true,
-      remove: true,
-      browsers: ['last 2 versions']
-    },
-    discardComments: {
-      removeAll: true
-    },
-    discardUnused: false,
-    mergeIdents: false,
-    reduceIdents: false,
-    safe: true,
-    sourcemap: true
-  })
-]
-
-export default webpackConfigProd
+export default Object.assign(config, webpackConfig)
