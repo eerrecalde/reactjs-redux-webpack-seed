3ba9ca48a950fbe9c464af21fc431b40230ecbb0
Emiliano Errecalde <eerrecalde@gmail.com>
2016-09-07 16:04:12 +0100

    Added Helmet to manage meta information

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router'
+import Helmet from "react-helmet"
 
 class HomePage extends React.Component {
   render () {
     return (
       <div className="jumbotron">
+        <Helmet
+          title="Starter kit | Home"
+        />
         <h1>Pluralsight Administration</h1>
         <p>React, Redux, and React Router</p>
         <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
